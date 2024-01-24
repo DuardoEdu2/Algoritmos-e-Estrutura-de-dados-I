c9ec47eedf3ea5ecfa2d9d02210d5a8d26b0ff45
Eduardo Abrantes <eduardopabrantes@gmail.com>
2024-01-24 10:53:38 -0300

    atividade de vatores estruturados questao 1

--- Atividades/LISTA DE EXERCÍCIOS - VETORES DE ESTRUTURA/Questao1.c
+++ Atividades/LISTA DE EXERCÍCIOS - VETORES DE ESTRUTURA/Questao1.c
@@ -6,7 +6,7 @@ typedef struct ingresso
 {
     /* data */
     char local[50];
-    char atração[50];
+    char atracao[50];
     float preco;
 
 }Ingresso;
@@ -14,20 +14,29 @@ typedef struct ingresso
 void preencher(Ingresso * ing){
 
     printf("Atração do ingresso:\t");
-    scanf(" %[^\n]", ing->atração);
-    
+    scanf(" %[^\n]", ing->atracao);
+
     printf("Local da sua atração:\t");
     scanf(" %[^\n]", ing->local);
 
     printf("Preço do seu ingresso:\t");
-    scanf("%d", ing->preco);
+    scanf("%f", &ing->preco);
 
+}
+
+void imprimi(Ingresso * ing){
+    printf("Atração: %s\nLocal: %s\nPreco: %.2f", ing->atracao, ing->local, ing->preco);
 }
 
 
 int main(void){
 
-    Ingresso ing = (Ingresso*)malloc(sizeof(Ingresso));
+    Ingresso *ing = (Ingresso*) malloc (sizeof(Ingresso));
+
+    preencher(ing);
+    imprimi(ing);
+
+    free(ing);
 
     return 0;
 
