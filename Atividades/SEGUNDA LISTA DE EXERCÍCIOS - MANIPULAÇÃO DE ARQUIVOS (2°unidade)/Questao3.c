e24ecf5c67a3c83fd59b5418d780d43bfcceb5fd
Eduardo Abrantes <eduardopabrantes@gmail.com>
2024-01-31 11:16:57 -0300

    Segunda atividade de de manipulação de arquivos

---
+++ Atividades/SEGUNDA LISTA DE EXERCÍCIOS - MANIPULAÇÃO DE ARQUIVOS (2°unidade)/Questao3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct frutas
+{
+    /* data */
+    char nome[50];
+    float preco;
+}Frutas;
+
+
+int main(void){
+
+    FILE *otp;
+    Frutas fru;
+    int value = 1;
+
+    //modo de escrita
+    otp = fopen("saida_q3.txt", "w");
+    if (otp == NULL)
+    {
+        printf("Erro na abertura do arquivo");
+        exit(1);
+    }
+    else{
+        printf("Arquivos Abertos\n");
+    }
+
+    while (value)
+    {
+        printf("Digite o nome: ");
+        scanf(" %[^\n]", fru.nome);
+    
+        printf("Digite o preço: ");
+        scanf("%f", &fru.preco);
+
+        fprintf(otp, "Nome e preço da fruta: %s, %.2f$\n", fru.nome, fru.preco);
+
+        printf("Deseja adicionar outra fruta? (s = 1/ n = 0)\n");
+        scanf("%d", &value);
+    }
+    
+
+    
+
+}
